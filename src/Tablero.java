b010b7a27ef4a24a765231d351f38413baa5054d
anxovaz <avazquezlorenzo@danielcastelao.org>
2026-04-20 14:42:58 +0200

    constantes Tablero

--- src/Tablero.java
+++ src/Tablero.java
@@ -1,2 +1,11 @@
 public class Tablero {
+    public static final int AGUA = 0;
+    public static final int TOCADO = 1;
+    public static final int HUNDIDO = 2;
+
+    public Tablero(){
+        //Se inicializa siempre a 10x10
+
+    }
+
 }
