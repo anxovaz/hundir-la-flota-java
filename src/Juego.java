705d7d31bfec482aa22bc1edf5606074b4b948ff
anxovaz <avazquezlorenzo@danielcastelao.org>
2026-04-20 14:34:15 +0200

    funcion mostrar_resultado

--- src/Juego.java
+++ src/Juego.java
@@ -2,6 +2,14 @@ class Juego{
     Tablero tablero = new Tablero();
 
     public Juego(){
-
+    }
+    public String mostrar_resultado(int resultado){
+        if(resultado == 0){
+            return "Agua";
+        }else if (resultado == 1){
+            return "Tocado";
+        }else{
+            return "Hundido";
+        }
     }
 }
