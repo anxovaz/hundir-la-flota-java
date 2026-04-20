e77b204e996d4b659afce2c270d78e8646a5b954
anxovaz <avazquezlorenzo@danielcastelao.org>
2026-04-20 14:39:13 +0200

    funcion lanzar_ataque()

--- src/Juego.java
+++ src/Juego.java
@@ -3,13 +3,22 @@ class Juego{
 
     public Juego(){
     }
-    public String mostrar_resultado(int resultado){
-        if(resultado == 0){
+    public String mostrar_resultado(int resultado) {
+        if (resultado == 0) {
             return "Agua";
-        }else if (resultado == 1){
+        } else if (resultado == 1) {
             return "Tocado";
-        }else{
+        } else {
             return "Hundido";
         }
     }
+
+    public void lanzar_ataque(int x, int y){
+        System.out.println("Atacando a " + x + "," + y);
+        int resultado = this.tablero.comprobar_impacto(x,y);
+        this.mostrar_resultado(resultado);
+    }
+
+
+
 }
