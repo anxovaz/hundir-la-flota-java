8ebc221cfb98c0727b53021ff4901995685a7184
anxovaz <avazquezlorenzo@danielcastelao.org>
2026-04-21 14:04:55 +0200

    Constructores y javadoc

--- src/Casilla.java
+++ src/Casilla.java
@@ -1,8 +1,24 @@
 public class Casilla {
-    public Casilla(Nave nave) {
+    /*
+    *Clase que representa una casilla dentro del tablero
+    * Esta clase tiene 2 constructores uno para "agua" y otro que acepta una nave
 
+    * Atributos:
+    - contenido: contiene o "agua" o una Nave
+    - estado: Booleano que indica si ya se lanzó un ataque anteriormente o no.
+
+    Métodos:
+    -comprobar_contenido() -> Comprueba si hay agua o Nave y devuelve true o false
+    -__init__ -> inizializador
+    -setter y getter -> para Casilla.__contenido (@property y @contenido.setter)
+    -recibirDisparo -> encargado de lanzar disparo a Nave
+     */
+     //por defecto agua
+    Boolean estado = false; //no se ha visitado
+    public Casilla(Nave nave) {
+        Nave contenido = nave;
     }
     public Casilla(String agua){
-
+        String contenido = "agua";
     }
 }
