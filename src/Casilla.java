e38dd4917a909c952f20e631b1b0f156bc60fb11
anxovaz <avazquezlorenzo@danielcastelao.org>
2026-04-22 11:39:05 +0200

    Clase casilla (#4)
    * Add Juego.java
    
    * Add Tablero.java
    
    * Add Nave.java
    
    * Add Casilla.java
    
    * funcion mostrar_resultado
    
    * funcion lanzar_ataque()
    
    * constantes Tablero
    
    * Constrctor Tablero
    
    * método colocar_nave
    
    * método comprobar_impacto
    
    * Constructor de nave y método recibir_disparo()
    
    * Constructores y javadoc
    
    * Fix contenido constructores
    
    * metodo comprobar_contenido()
    
    * metodo recibir_disparo()

--- src/Casilla.java
+++ src/Casilla.java
@@ -1,3 +1,60 @@
 public class Casilla {
+    /*
+    *Clase que representa una casilla dentro del tablero
+    * Esta clase tiene 2 constructores uno para "agua" y otro que acepta una nave
+
+    * Atributos:
+    - contenido: contiene o "agua" o una Nave
+    - estado: Booleano que indica si ya se lanzó un ataque anteriormente o no.
+
+    Métodos:
+    -comprobar_contenido() -> Comprueba si hay agua o Nave y devuelve true o false
+    -__init__ -> inizializador
+    -recibirDisparo -> encargado de lanzar disparo a Nave
+     */
+     //por defecto agua
+    public Boolean estado = false; //no se ha visitado
+    public Nave contenido;
+    public Casilla(Nave nave) {
+        this.contenido = nave;
+    }
+    public Casilla(String agua){
+        this.contenido = null;
+    }
+
+    public boolean comprobar_contenido(){ //comprueba si es agua o nave
+        if(this.contenido == null){
+            return false;
+        }else{
+            return true;
+        }
+    }
+    public int recibirDisparo(){
+        /*
+        recibirDisparo devuelve:
+        0 en caso de agua
+        1 en caso de nave
+        -1 en caso de que el usuario haya atacado dos o más veces a la misma casilla y
+        2  si está hundido
+         */
+
+        if(this.estado == false){//si no se le ha disparado antes
+            this.estado = true;
+            if(comprobar_contenido() == true){//si contiene una nave
+                this.contenido.recibir_disparo(); //se lanza un disparo
+                if(this.contenido.tamanho <= 0){ //si no tiene vidas
+                    return 2;
+                }else{ //tiene vidas
+                    return 1;
+                }
+
+            }else { //agua
+                return 0;
+
+            }
+            }else{
+            return -1;
+        }
+    }
 
 }
